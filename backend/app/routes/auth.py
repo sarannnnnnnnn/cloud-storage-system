544974e25da2b4b025327f4c8f36a4a73d22e26c
sarannnnnnnn <saran16062007@gmail.com>
2026-07-13 22:15:48 +0530

    jwt authentication and protected routes

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserLogin
 from app.utils.security import hash_password, verify_password
+from app.utils.jwt_handler import create_access_token, verify_access_token
 
 router = APIRouter()
+security = HTTPBearer()
 
 
 @router.post("/register")
@@ -27,21 +30,50 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         "message": "User registered successfully"
     }
 
+
 @router.post("/login")
 def login(user: UserLogin, db: Session = Depends(get_db)):
 
+    # Search user by email
     db_user = db.query(User).filter(User.email == user.email).first()
 
+    # Email not found
     if not db_user:
         return {
             "message": "Invalid email or password"
         }
 
+    # Password is incorrect
     if not verify_password(user.password, db_user.password):
         return {
             "message": "Invalid email or password"
         }
 
+    # Create JWT Token
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+
+    # Return JWT Token
     return {
-        "message": "Login successful"
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/profile")
+def profile(credentials: HTTPAuthorizationCredentials = Depends(security)):
+
+    token = credentials.credentials
+
+    payload = verify_access_token(token)
+
+    if payload is None:
+        return {
+            "message": "Invalid Token"
+        }
+
+    return {
+        "message": "Protected Route",
+        "user": payload["sub"]
     }
